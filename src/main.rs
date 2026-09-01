48bb2bcbcf1bcd02e55f8f8f748c6e429e3b3ba3
Christoffer Andersson <chan8@kth.se>
2026-09-01 15:44:47 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,12 @@ fn main() {
 
     println!("--- Call function normally ---");
     println!();
-    // call function normally (works)
     hashbrown_dll_bug_macos::use_hashbrown_hashmap(&std_hashmap, &hashbrown_hashmap);
 
     println!();
 
     println!("--- Call function through dll ---");
     println!();
-    // Dll function (crashes)
     let dll = Library::open("./libhashbrown_dll_bug_macos.dylib").expect("could not open dll");
     let func = unsafe {
         dll.symbol::<fn(&std::collections::HashMap<char, u32>, &hashbrown::HashMap<char, u32>)>(
