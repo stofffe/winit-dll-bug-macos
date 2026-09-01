36c757ed4d1e5df7f0b6bdfcf189d457087f9e3e
Christoffer Andersson <chan8@kth.se>
2026-09-01 15:43:25 +0200

    add std hashmap for comparison

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 #[unsafe(no_mangle)]
-pub fn use_hashbrown_hashmap(hashmap: &hashbrown::HashMap<char, u32>) {
-    println!("HashMap = {:?}", hashmap);
-    println!("HashMap lookup of a = {:?}", hashmap.get(&'a'));
-    println!("HashMap lookup of x = {:?}", hashmap.get(&'x'));
+pub fn use_hashbrown_hashmap(
+    std_hashmap: &std::collections::HashMap<char, u32>,
+    hashbrown_hashmap: &hashbrown::HashMap<char, u32>,
+) {
+    println!("std HashMap = {:?}", std_hashmap);
+    println!("std HashMap lookup of a = {:?}", std_hashmap.get(&'a'));
+    println!("std HashMap lookup of x = {:?}", std_hashmap.get(&'x'));
+
+    println!();
+
+    println!("hashbrown HashMap = {:?}", hashbrown_hashmap);
+    println!(
+        "hashbrown HashMap lookup of a = {:?}",
+        hashbrown_hashmap.get(&'a')
+    );
+    println!(
+        "hashbrown HashMap lookup of x = {:?}",
+        hashbrown_hashmap.get(&'x')
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,26 @@ fn main() {
     hashbrown_hashmap.insert('a', 1);
     hashbrown_hashmap.insert('x', 32);
 
-    println!("Call function normally");
+    let mut std_hashmap = std::collections::HashMap::new();
+    std_hashmap.insert('a', 1);
+    std_hashmap.insert('x', 32);
+
+    println!("--- Call function normally ---");
+    println!();
     // call function normally (works)
-    hashbrown_dll_bug_macos::use_hashbrown_hashmap(&hashbrown_hashmap);
+    hashbrown_dll_bug_macos::use_hashbrown_hashmap(&std_hashmap, &hashbrown_hashmap);
 
     println!();
 
-    println!("Call function through dll");
+    println!("--- Call function through dll ---");
+    println!();
     // Dll function (crashes)
     let dll = Library::open("./libhashbrown_dll_bug_macos.dylib").expect("could not open dll");
-    let func = unsafe { dll.symbol::<fn(&hashbrown::HashMap<char, u32>)>("use_hashbrown_hashmap") }
-        .unwrap();
-    func(&hashbrown_hashmap);
+    let func = unsafe {
+        dll.symbol::<fn(&std::collections::HashMap<char, u32>, &hashbrown::HashMap<char, u32>)>(
+            "use_hashbrown_hashmap",
+        )
+    }
+    .unwrap();
+    func(&std_hashmap, &hashbrown_hashmap);
 }
